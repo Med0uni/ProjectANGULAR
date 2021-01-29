4a37407b7b2ca1a4a5b934aa741ff195c6850161
Mohamed Ouni <medouni13@gmail.com>
2021-01-29 11:31:38 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SessionsComponent } from './sessions/sessions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ParticipantsComponent } from './participants/participants.component';
+import { FormateursComponent } from './formateurs/formateurs.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'sessions', component: SessionsComponent },
+  { path: 'formateurs', component: FormateursComponent },
+  { path: 'participants', component: ParticipantsComponent }
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  
+  imports: [RouterModule.forRoot(routes, {enableTracing: true}),
+  RouterModule.forRoot(routes)],
+  
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { SessionsComponent } from './sessions/sessions.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ParticipantsComponent } from './participants/participants.component';
+import { FormateursComponent } from './formateurs/formateurs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    SessionsComponent,
+    DashboardComponent,
+    ParticipantsComponent,
+    FormateursComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
